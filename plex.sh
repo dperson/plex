234278996ba9b5a047fec0e20e1c29b71120d61e
David Personette <dperson@gmail.com>
2015-02-07 18:28:29 -0500

    plex.sh try skipping dbus and avahi

--- plex.sh
+++ plex.sh
@@ -68,13 +68,6 @@ else
     APPDIR="/config/Library/Application Support/Plex Media Server"
     rm -f "$APPDIR"/*.pid
     rm -rf /var/run/*
-    mkdir -p /var/run/dbus
-    chown messagebus. /var/run/dbus
-    dbus-uuidgen --ensure
-    dbus-daemon --system --fork
-    sleep 1
-    avahi-daemon -D
-    sleep 1
     chown plex. -Rh /config
     chown plex. /data
     su -l plex -c "/usr/sbin/start_pms &" >/dev/null 2>&1
